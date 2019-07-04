e039b9bdbd219fbce33eb6e94c881c9a29477154
ViktorAH14 <ermolovva@gmail.com>
2019-07-04 16:35:24 +0300

    Implemented designer form class ViewCatalogForm.

--- viewcatalogform.cpp
+++ viewcatalogform.cpp
@@ -1,5 +1,6 @@
 #include "viewcatalogform.h"
 #include "ui_viewcatalogform.h"
+#include "tableiomapper.h"
 
 ViewCatalogForm::ViewCatalogForm(QWidget *parent) :
     QDialog(parent),
@@ -11,4 +12,33 @@ ViewCatalogForm::ViewCatalogForm(QWidget *parent) :
 ViewCatalogForm::~ViewCatalogForm()
 {
     delete ui;
+}
+
+void ViewCatalogForm::setMapper(TableIOMapper *mapper)
+{
+    this->mapper = mapper;
+    mapper->setView(ui->tableView);
+    mapper->setColumnName(0, windowTitle());
+    ui->tableView->setColumnWidth(0, 270);
+    //    ui->tableView->hideColumn(1);
+}
+
+void ViewCatalogForm::addItem()
+{
+    if (!mapper.isNull())
+        mapper->newRow();
+}
+
+void ViewCatalogForm::deleteItem()
+{
+    if (!mapper.isNull())
+        mapper->deleteRow(ui->tableView->currentIndex().row());
+}
+
+void ViewCatalogForm::save()
+{
+    if (!mapper.isNull()) {
+//        ui->tableView->submit(); BUG ui->tableView-.submit();
+        mapper->save();
+    }
 }
