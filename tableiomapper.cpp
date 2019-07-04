46ab73dd3b70fec24ddf27ac7d2db628b84fef6e
ViktorAH14 <ermolovva@gmail.com>
2019-07-04 13:53:42 +0300

    Modified tableiomapper.h

--- tableiomapper.cpp
+++ tableiomapper.cpp
@@ -1,7 +1,22 @@
 #include "tableiomapper.h"
 
-TableIOMapper::TableIOMapper(QObject *parent) : QObject(parent)
-{
+#include <QDataWidgetMapper>
+#include <QSqlRelationalTableModel>
+#include <QSqlRelationalDelegate>
+#include <QAbstractItemView>
+#include <QSqlRelation>
 
+TableIOMapper::TableIOMapper(Table *table, QObject *parent) : QObject(parent)
+{
+    m_table = table;
+    mapper = new QDataWidgetMapper(this);
+    mapper->setModel(table->m_model);
+    mapper->setSubmitPolicy(QDataWidgetMapper::AutoSubmit);
+    mapper->setItemDelegate(new QSqlRelationalDelegate());
+}
+
+TableIOMapper::~TableIOMapper()
+{
+    delete mapper;
 }
 
--- tableiomapper.h
+++ tableiomapper.h
@@ -1,17 +1,52 @@
 #ifndef TABLEIOMAPPER_H
 #define TABLEIOMAPPER_H
 
+#include "table.h"
+
 #include <QObject>
+#include <QMap>
+
+class QDataWidgetMapper;
+class QWidget;
+class QComboBox;
+class QListView;
+class QAbstractItemView;
+class QComboBox;
 
 class TableIOMapper : public QObject
 {
     Q_OBJECT
 public:
-    explicit TableIOMapper(QObject *parent = 0);
+    explicit TableIOMapper(Table *table, QObject *parent = 0);
+    ~TableIOMapper();
 
-signals:
+    bool selectRow(int num);
+    bool selectNextRow();
+    bool selectPrevRow();
+    void selectFirstRow();
+    void selectLastRow();
+    int rowsCount();
+    int currentRow();
+    void addDataWidget(QWidget *widget, int column);
+    void addDataWidget(Qwidget *widget, const QString &columnName);
+    void addRelationalComboBox(QComboBox *comboBox, int column);
+    void addRelationalComboBox(QComboBox *comboBox, const QString &columnName);
+    void reloadRelation(QComboBox *comboBox);
+    void reloadRelations();
+    void clearDataWidgets();
+    void setView(QAbstractItemView *view);
+    void setColumnName(int column, const QString &name);
 
 public slots:
+    void newRow();
+    void save();
+    void canselChanges();
+    void refresh();
+    void deleteRow(int num);
+
+protected:
+    QDataWidgetMapper *mapper;
+    Table *m_table;
 };
 
 #endif // TABLEIOMAPPER_H
